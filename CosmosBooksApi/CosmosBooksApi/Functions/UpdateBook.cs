8bf9c2ebd7ec39a7bb805eb2338de74d9adf1584
willvelida <willvelida@hotmail.co.uk>
2021-02-27 21:40:57 +1300

    Adding UpdateBook endpoint

---
+++ CosmosBooksApi/CosmosBooksApi/Functions/UpdateBook.cs
@@ -0,0 +1,70 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using CosmosBooksApi.Repositories;
+using CosmosBooksApi.Models;
+
+namespace CosmosBooksApi.Functions
+{
+    public class UpdateBook
+    {
+        private readonly ILogger<UpdateBook> _logger;
+        private readonly IBookRepository _bookRepository;
+
+        public UpdateBook(
+            ILogger<UpdateBook> logger,
+            IBookRepository bookRepository)
+        {
+            _logger = logger;
+            _bookRepository = bookRepository;
+        }
+
+        [FunctionName(nameof(UpdateBook))]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "Book/{id}")] HttpRequest req,
+            string id)
+        {
+            IActionResult result;
+
+            try
+            {
+                var bookToUpdate = await _bookRepository.GetBook(id);
+
+                if (bookToUpdate == null)
+                {
+                    result = new StatusCodeResult(StatusCodes.Status404NotFound);
+                }
+
+                var input = await new StreamReader(req.Body).ReadToEndAsync();
+
+                var updateBookRequest = JsonConvert.DeserializeObject<Book>(input);
+
+                Book updatedBook = new Book
+                {
+                    Id = id,
+                    BookName = bookToUpdate.BookName,
+                    Author = updateBookRequest.Author,
+                    Category = updateBookRequest.Category,
+                    Price = updateBookRequest.Price
+                };
+
+                await _bookRepository.UpdateBook(id, updatedBook);
+
+                result = new StatusCodeResult(StatusCodes.Status202Accepted);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Internal Server Error: {ex.Message}");
+                result = new StatusCodeResult(StatusCodes.Status500InternalServerError);
+            }
+
+            return result;
+        }
+    }
+}
